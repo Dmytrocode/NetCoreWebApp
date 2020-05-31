30b362886d229560488321972bfafc151a1c629a
Dmytro Kavetskyy <dmytrocontact@gmail.com>
2020-05-30 20:50:42 -0600

    Added Razor Pages, Createrd and setup a Controller, Added views Home/About/Contact and further modifications

--- Startup.cs
+++ Startup.cs
@@ -15,14 +15,32 @@ namespace NetCoreApp
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        [Obsolete]
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            app.UseDefaultFiles();
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/error");
+            }
             app.UseStaticFiles();
-            
+            app.UseNodeModules();
+
+            app.UseRouting();
+
+            app.UseEndpoints(cfg =>
+            {
+                cfg.MapControllerRoute("Fallback",
+                    "{controller}/{action}/{id?}",
+                    new { controller = "App", action = "Index" });
+            });
         }
     }
 }
---
+++ wwwroot/js/index.js
@@ -0,0 +1,28 @@
+﻿
+$(document).ready(function () {
+
+
+    var theForm = $("#theForm");
+
+    var button = $("#buyButton");
+    button.on("click", function () {
+
+        console.log("Buy item");
+    });
+
+    var prodInfo = $(".product-props li");
+
+    prodInfo.on("click", function () {
+
+        console.log("You clicked" + $(this).text());
+    });
+
+    var $loginToggle = $("#loginToggle");
+    var $popupForm = $(".popup-form");
+
+    $loginToggle.on("click", function () {
+
+        $popupForm.toggle(400);
+    })
+
+});
